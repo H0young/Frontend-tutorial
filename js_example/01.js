0e1b8f4ca0365fe1c5abb8cc468d59f567c8565e
Hoyoung Kim <ghdud9606@gmail.com>
2020-06-28 02:22:51 +0900

    Practice js 01

---
+++ js_example/01.js
@@ -0,0 +1,24 @@
+let value = 1
+console.log(value)
+
+let text = 'hello'
+let name = "Nykong"
+
+let good = true
+let something = null
+let unknown = undefined
+
+let a = 1 + 5
+console.log(a)
+
+let b = 1
+console.log(b)
+console.log(++b)
+b += b
+// 주석처리
+// not = !
+// and = &&
+// or = ||
+
+const A = !true
+const B = true && true
