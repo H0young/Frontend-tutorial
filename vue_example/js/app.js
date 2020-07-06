e3277ea84fba61a1c1cb5d6cbc70d4de82ea42ad
Hoyoung Kim <ghdud9606@gmail.com>
2020-07-06 17:59:41 +0900

    Practice Vue.js

--- vue_example/js/app.js
+++ vue_example/js/app.js
@@ -1,11 +1,19 @@
 import SearchModel from './models/SearchModel.js'
+import KeywordModel from './models/KeywordModel.js'
 
 new Vue({
     el: '#app',
     data: {
         query: '', //입력 데이터 받아 저장, v-model: 양 방향
         submitted: false,
-        searchResult: []
+        tabs: ['추천 검색어', '최근 검색어'],
+        selectedTab: '',
+        keywords: [],
+        searchResult: [],
+    },
+    created() {
+        this.selectedTab = this.tabs[0]
+        this.fetchKeyword()
     },
     methods: {
         onSubmit(e) {
@@ -17,6 +25,18 @@ new Vue({
         onReset(e) {
             this.resetForm()
         },
+        onClickTab(tab) {
+            this.selectedTab = tab
+        },
+        onClickKeyword(keyword) {
+            this.query = keyword
+            this.search()
+        },
+        fetchKeyword() {
+            KeywordModel.list().then(data => {
+                this.keywords = data
+            })
+        },
         search(e) {
             SearchModel.list().then(data => {
                 this.submitted = true
@@ -26,8 +46,8 @@ new Vue({
         },
         resetForm(e) {
             this.query = ''
-            // todo 검색결과 숨기기
-            debugger
+            this.submitted = false
+            this.searchResult = []
         }
     }
 })
