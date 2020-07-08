cad03732781fe0803db04a44623b5d9635656047
Hoyoung Kim <ghdud9606@gmail.com>
2020-07-08 20:35:57 +0900

    Practice Single Component 02

--- single_component_example/src/App.vue
+++ single_component_example/src/App.vue
@@ -3,8 +3,31 @@
     <header>
       <h2 class="container">검색</h2>
     </header>
+
     <div class="container">
       <search-form v-bind:value="query" v-on:@submit="onSubmit" v-on:@reset="onReset"></search-form>
+      <div class="content">
+        <div v-if="submitted">
+          <search-result v-bind:data="searchResult" v-bind:query="query"></search-result>
+        </div>
+
+        <div v-else>
+          <tabs v-bind:tabs="tabs" v-bind:selected-tab="selectedTab" v-on:@change="onClickTab"></tabs>
+
+          <div v-if="selectedTab === tabs[0]">
+            <list v-bind:data="keywords" type="keywords" v-on:@click="onClickKeyword"></list>
+          </div>
+
+          <div v-else>
+            <list
+              v-bind:data="history"
+              type="history"
+              v-on:@click="onClickKeyword"
+              v-on:@remove="onClickRemoveHistory"
+            ></list>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -15,6 +38,9 @@ import KeywordModel from "./models/KeywordModel.js";
 import HistoryModel from "./models/HistoryModel.js";
 
 import FormComponent from "./components/FormComponent.vue";
+import ListComponent from "./components/ListComponent.vue";
+import TabComponent from "./components/TabComponent.vue";
+import ResultComponent from "./components/ResultComponent.vue";
 
 export default {
   name: "app",
@@ -30,7 +56,15 @@ export default {
     };
   },
   components: {
-    "search-form": FormComponent
+    "search-form": FormComponent,
+    "search-result": ResultComponent,
+    list: ListComponent,
+    tabs: TabComponent
+  },
+  created() {
+    this.selectedTab = this.tabs[0];
+    this.fetchKeyword();
+    this.fetchHistory();
   },
   methods: {
     onSubmit(query) {
